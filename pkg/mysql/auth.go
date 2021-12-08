fbc97952b85c45c31e20ab54b7888702e87a5d87
scott lewis <digitalkosmos.sl@gmail.com>
2021-12-08 14:31:31 +0800

    fix: license header

--- pkg/mysql/auth.go
+++ pkg/mysql/auth.go
@@ -1,3 +1,4 @@
+//
 // Licensed to Apache Software Foundation (ASF) under one or more contributor
 // license agreements. See the NOTICE file distributed with
 // this work for additional information regarding copyright
@@ -15,13 +16,6 @@
 // specific language governing permissions and limitations
 // under the License.
 //
-// Go MySQL Driver - A MySQL-Driver for Go's database/sql package
-//
-// Copyright 2018 The Go-MySQL-Driver Authors. All rights reserved.
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package mysql
 
