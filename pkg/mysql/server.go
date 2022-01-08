04b9ae74c261eb95c64c7907fb70a08d28db5e58
scott lewis <digitalkosmos.sl@gmail.com>
2022-01-08 14:50:00 +0800

    fix:when client deprecate eof packet, should write ok packet instead

--- pkg/mysql/server.go
+++ pkg/mysql/server.go
@@ -531,7 +531,11 @@ func (l *Listener) ExecuteCommand(c *Conn, ctx *proto.Context) error {
 			if err != nil {
 				return err
 			}
-			return c.writeEOFPacket(c.StatusFlags, warn)
+			if err := c.writeEndResult(l.capabilities, false, 0, 0, warn); err != nil {
+				log.Errorf("Error writing result to %s: %v", c, err)
+				return err
+			}
+			return nil
 		}()
 		if err != nil {
 			return err
@@ -657,7 +661,11 @@ func (l *Listener) ExecuteCommand(c *Conn, ctx *proto.Context) error {
 			if err != nil {
 				return err
 			}
-			return c.writeEOFPacket(c.StatusFlags, warn)
+			if err := c.writeEndResult(l.capabilities, false, 0, 0, warn); err != nil {
+				log.Errorf("Error writing result to %s: %v", c, err)
+				return err
+			}
+			return nil
 		}()
 		if err != nil {
 			return err
