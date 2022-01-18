714af8206164b620b64936a32b94d450d1d00b0e
luky116 <luky116@126.com>
2022-01-19 01:06:40 +0800

    add new config struct

--- pkg/config/db.go
+++ pkg/config/db.go
@@ -48,6 +48,92 @@ type (
 		IdleTimeout    time.Duration   `yaml:"idle_timeout" json:"_"`            // close backend direct connection after idle_timeout
 		IdleTimeoutStr string          `yaml:"_" json:"idle_timeout"`
 	}
+
+	AranaConfig struct {
+		Kind      string     `yaml:"kind" json:"kind"`
+		Metadata  Metadata   `yaml:"metadata" json:"metadata"`
+		Data      ConfigData `yaml:"config_data" json:"config_data"`
+		Listeners []Listener `yaml:"listeners" json:"listeners"`
+		Executors []Executor `yaml:"executors" json:"executors"`
+	}
+
+	Metadata struct {
+		Name string `yaml:"name" json:"name"`
+	}
+
+	ConfigData struct {
+		ApiVersion         string              `yaml:"api_version" json:"api_version"`
+		DataSourceClusters []DataSourceCluster `yaml:"data_source_clusters" json:"data_source_clusters"`
+		ShardingRules      []ShardingRule      `yaml:"sharding_rules" json:"sharding_rules"`
+	}
+
+	DataSourceCluster struct {
+		Name        string         `yaml:"name" json:"name"`
+		Type        DataSourceType `yaml:"type" json:"type"`
+		SqlMaxLimit int            `yaml:"sql_max_limit" json:"sql_max_limit"`
+		Tenant      string         `yaml:"tenant" json:"tenant"`
+		ConnProps   ConnProps      `yaml:"conf_props" json:"conf_props"`
+		Groups      []Group        `yaml:"groups" json:"groups"`
+	}
+
+	ConnProps struct {
+		Capacity    int           `yaml:"capacity" json:"capacity"`         // connection pool capacity
+		MaxCapacity int           `yaml:"max_capacity" json:"max_capacity"` // max connection pool capacity
+		IdleTimeout time.Duration `yaml:"idle_timeout" json:"idle_timeout"` // close backend direct connection after idle_timeout
+	}
+
+	Group struct {
+		Name    string `yaml:"name" json:"name"`
+		AtomDbs []Dsn  `yaml:"atom_dbs" json:"atom_dbs"`
+	}
+
+	Dsn struct {
+		Host     string         `yaml:"host" json:"host"`
+		Port     int            `yaml:"port" json:"port"`
+		Username string         `yaml:"username" json:"username"`
+		Password string         `yaml:"password" json:"password"`
+		Database string         `yaml:"database" json:"database"`
+		DsnProps DsnProp        `yaml:"props" json:"props"`
+		Role     DataSourceRole `yaml:"role" json:"role"`
+		Weight   string         `yaml:"weight" json:"weight"`
+	}
+
+	DsnProp struct {
+		Timeout      int    `yaml:"timeout" json:"timeout"`
+		ReadTimeout  string `yaml:"read_timeout" json:"read_timeout"`
+		WriteTimeout string `yaml:"write_timeout" json:"write_timeout"`
+		Loc          string `yaml:"loc" json:"loc"`
+		Charset      string `yaml:"charset" json:"charset"`
+	}
+
+	ShardingRule struct {
+		Tables []Table `yaml:"tables" json:"tables"`
+	}
+
+	Table struct {
+		Name            string   `yaml:"name" json:"name"`
+		AllowFullScan   bool     `yaml:"allow_full_scan" json:"allow_full_scan"`
+		ActualDataNodes string   `yaml:"actual_data_nodes" json:"actual_actual_nodes"`
+		DbRules         []Rule   `yaml:"db_rules" json:"db_rules"`
+		TblRules        []Rule   `yaml:"tbl_rules" json:"tbl_rules"`
+		TblProps        TblProps `yaml:"other_props" json:"other_props"`
+		Topology        Topology `yaml:"topology" json:"topology"`
+		ShadowTopology  Topology `yaml:"shadow_topology" json:"shadow_topology"`
+	}
+
+	Rule struct {
+		Column string `yaml:"column" json:"column"`
+		Expr   string `yaml:"expr" json:"expr"`
+	}
+
+	TblProps struct {
+		SqlMaxLimit int `yaml:"sql_max_limit" json:"sql_max_limit"` // connection pool capacity
+	}
+
+	Topology struct {
+		DbPattern  string `yaml:"db_pattern" json:"db_pattern"`
+		TblPattern string `yaml:"tbl_pattern" json:"tbl_pattern"`
+	}
 )
 
 const (
