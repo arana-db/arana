829bed4d0aa5e8af2925c0d33b3a81d5fbdc2515
tydhot <583125614@qq.com>
2022-01-26 00:42:59 +0800

    关于handshake相应解析的测试用例

--- pkg/mysql/client_test.go
+++ pkg/mysql/client_test.go
@@ -23,6 +23,8 @@ import (
 )
 
 import (
+	"github.com/dubbogo/arana/pkg/constants/mysql"
+	"github.com/dubbogo/arana/pkg/mysql/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -84,4 +86,94 @@ func TestDSNWithParam(t *testing.T) {
 
 	assert.True(t, cfg.AllowAllFiles)
 	assert.True(t, cfg.AllowCleartextPasswords)
+}
+
+func TestParseInitialHandshakePacket(t *testing.T) {
+	dsn := "admin:123456@tcp(127.0.0.1:3306)/pass?allowAllFiles=true&allowCleartextPasswords=true"
+	cfg, _ := ParseDSN(dsn)
+	conn := &BackendConnection{conf: cfg}
+	conn.c = newConn(new(mockConn))
+	data := make([]byte, 4)
+	data[0] = mysql.ErrPacket
+	data[1] = 400 & 0xff
+	data[2] = 400 >> 8
+	data[3] = 65
+	_, _, _, err := conn.parseInitialHandshakePacket(data)
+	assert.Error(t, err)
+	assert.Equal(t, "immediate error from server errorCode=400 errorMsg=A", err.(*errors.SQLError).Message)
+
+	data[0] = mysql.ProtocolVersion - 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Error(t, err)
+
+	data = make([]byte, 2)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no server version", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 6)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no connection id", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 14)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no auth-plugin-Content-part-1", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 15)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no filler", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 16)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no capability flags (lower 2 bytes)", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 18)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	length, _, _, _ := conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, uint32(0), length)
+
+	data = make([]byte, 19)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no status flags", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 21)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no capability flags (upper 2 bytes)", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 23)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, _, err = conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "parseInitialHandshakePacket: packet has no length of auth-plugin-Content filler", err.(*errors.SQLError).Message)
+
+	data = make([]byte, 24)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	_, _, password, _ := conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, mysql.MysqlNativePassword, password)
+
+	data = make([]byte, 37)
+	data[0] = mysql.ProtocolVersion
+	data[1] = 1
+	data[17] = 255
+	data[21] = 255
+	data[23] = 9
+	data[35] = 65
+	_, authData, authName, _ := conn.parseInitialHandshakePacket(data)
+	assert.Equal(t, "A", authName)
+	assert.Equal(t, "\x00\x00\x00\x00\x00\x00\x00\x00", string(authData))
 }
