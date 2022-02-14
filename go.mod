564ff2f0437dc0cbb2c378b16d0aadb195d74909
luky116_Liuyuecai <luky116@126.com>
2022-02-14 09:33:11 +0800

    feat: add group by stream merge logic

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/pkg/errors v0.9.1
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/shopspring/decimal v1.3.1
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.14.0
---
+++ pkg/merge/impl/group_by/group_by_value.go
@@ -0,0 +1,63 @@
+//
+// Licensed to Apache Software Foundation (ASF) under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Apache Software Foundation (ASF) licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
+package group_by
+
+import (
+	"fmt"
+)
+
+import (
+	"github.com/dubbogo/arana/pkg/proto"
+)
+
+type GroupByValue struct {
+	groupByColumns []string
+	groupByValues  []interface{}
+}
+
+func NewGroupByValue(groupByColumns []string, row proto.Row) *GroupByValue {
+	return &GroupByValue{
+		groupByColumns: groupByColumns,
+		groupByValues:  buildGroupValues(groupByColumns, row),
+	}
+}
+
+func buildGroupValues(groupByColumns []string, row proto.Row) []interface{} {
+	values := make([]interface{}, 0)
+
+	for _, column := range groupByColumns {
+		value, err := row.GetColumnValue(column)
+		if err != nil {
+			panic("get column value error:" + err.Error())
+		}
+		values = append(values, value)
+	}
+	return values
+}
+
+func (g *GroupByValue) equals(row proto.Row) bool {
+	values := buildGroupValues(g.groupByColumns, row)
+	for k := range values {
+		if fmt.Sprintf("%v", values[k]) != fmt.Sprintf("%v", g.groupByValues[k]) {
+			return false
+		}
+	}
+	return true
+}
