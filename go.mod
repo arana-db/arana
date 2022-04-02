8c67d7d432b132055ca7d4c8912f056ce2c415a1
Zonglei Dong <dongzonglei@apache.org>
2022-04-02 09:42:55 +0800

    Optimize test code.

--- go.mod
+++ go.mod
@@ -12,13 +12,11 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/mock v1.5.0
 	github.com/hashicorp/golang-lru v0.5.4
-	github.com/jmoiron/sqlx v1.3.4
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/lestrrat-go/strftime v1.0.5
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/pkg/errors v0.9.1
-	github.com/qreasio/go-starter-kit v0.0.0-20200823044549-4955f5c7be23
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0
--- test/testcontainer_mysql_test.go
+++ test/testcontainer_mysql_test.go
@@ -1,14 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package test_test
 
 import (
 	"database/sql"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
 )
 
 import (
@@ -22,11 +38,13 @@ var (
 
 func TestMain(m *testing.M) {
 	var err error
-	var terminateContainer func() // variable to store function to terminate container
+	// variable to store function to terminate container
+	var terminateContainer func()
 	terminateContainer, db, err = test.SetupMySQLContainer()
-	defer terminateContainer() // make sure container will be terminated at the end
+	// make sure container will be terminated at the end
+	defer terminateContainer()
 	if err != nil {
-		log.Error("failed to setup MySQL container")
+		log.Error("Failed to setup MySQL container")
 		panic(err)
 	}
 	os.Exit(m.Run())
