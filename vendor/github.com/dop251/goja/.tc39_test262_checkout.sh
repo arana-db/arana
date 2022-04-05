bea2cdd0acdbfe0abc5faa5d8c1bd62a34b0386b
chuntaojun <liaochuntao@live.com>
2022-04-05 21:51:17 +0800

    feat: feat issue #62

--- vendor/github.com/dop251/goja/.tc39_test262_checkout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sha=26f1f4567ee7e33163d961c867d689173cbb9065 # this is just the commit it was last tested with
-mkdir -p testdata/test262
-cd testdata/test262
-git init
-git remote add origin https://github.com/tc39/test262.git
-git fetch origin --depth=1 "${sha}"
-git reset --hard "${sha}"
-cd -
--- vendor/github.com/pingcap/errors/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/pingcap/errors
-
-go 1.14
-
-require (
-	github.com/stretchr/testify v1.4.0
-	go.uber.org/atomic v1.6.0
-)
