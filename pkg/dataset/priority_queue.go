1d228711fb4c6dae58ea0e18b55b3c5309cebc5e
Zonglei Dong <dongzonglei@apache.org>
2022-06-27 18:39:18 +0800

    Feat: add select order by feature.

--- pkg/dataset/priority_queue.go
+++ pkg/dataset/priority_queue.go
@@ -20,6 +20,7 @@ package dataset
 import (
 	"container/heap"
 	"fmt"
+	"time"
 )
 
 import (
@@ -40,6 +41,10 @@ type OrderByItem struct {
 	Desc   bool
 }
 
+type orderByValue struct {
+	orderValues map[string]interface{}
+}
+
 func NewPriorityQueue(rows []*RowItem, orderByItems []OrderByItem) *PriorityQueue {
 	pq := &PriorityQueue{
 		rows:         rows,
@@ -54,20 +59,22 @@ func (pq *PriorityQueue) Len() int {
 }
 
 func (pq *PriorityQueue) Less(i, j int) bool {
+	orderValues1 := &orderByValue{
+		orderValues: make(map[string]interface{}),
+	}
+	orderValues2 := &orderByValue{
+		orderValues: make(map[string]interface{}),
+	}
 	for _, item := range pq.orderByItems {
 		row1 := pq.rows[i]
 		row2 := pq.rows[j]
 
 		val1, _ := row1.row.Get(item.Column)
 		val2, _ := row2.row.Get(item.Column)
-		if val1 != val2 {
-			if item.Desc {
-				return fmt.Sprintf("%v", val1) > fmt.Sprintf("%v", val2)
-			}
-			return fmt.Sprintf("%v", val1) < fmt.Sprintf("%v", val2)
-		}
+		orderValues1.orderValues[item.Column] = val1
+		orderValues2.orderValues[item.Column] = val2
 	}
-	return true
+	return orderValues1.Compare(orderValues2, pq.orderByItems) > 0
 }
 
 func (pq *PriorityQueue) Swap(i, j int) {
@@ -88,4 +95,111 @@ func (pq *PriorityQueue) Pop() interface{} {
 	item := old.rows[n-1]
 	pq.rows = old.rows[0 : n-1]
 	return item
+}
+
+func (value *orderByValue) Compare(compareVal *orderByValue, orderByItems []OrderByItem) int8 {
+	for _, item := range orderByItems {
+		compare := compareTo(value.orderValues[item.Column], compareVal.orderValues[item.Column], item.Desc)
+		if compare == 0 {
+			continue
+		}
+		return compare
+	}
+	return 0
+}
+
+func compareTo(thisValue, otherValue interface{}, desc bool) int8 {
+	if thisValue == nil && otherValue == nil {
+		return 0
+	}
+	if thisValue == nil {
+		return 1
+	}
+	if otherValue == nil {
+		return -1
+	}
+	// TODO Deal with case sensitive.
+	switch thisValue.(type) {
+	case string:
+		return compareString(fmt.Sprintf("%v", thisValue), fmt.Sprintf("%v", otherValue), desc)
+	case int8, int16, int32, int64:
+		return compareInt64(thisValue.(int64), otherValue.(int64), desc)
+	case uint8, uint16, uint32, uint64:
+		return compareUint64(thisValue.(uint64), otherValue.(uint64), desc)
+	case float32, float64:
+		return compareFloat64(thisValue.(float64), otherValue.(float64), desc)
+	case time.Time:
+		return compareTime(thisValue.(time.Time), otherValue.(time.Time), desc)
+	}
+	return 0
+}
+
+func compareTime(thisValue, otherValue time.Time, desc bool) int8 {
+	if desc {
+		if thisValue.After(otherValue) {
+			return 1
+		}
+		return -1
+	} else {
+		if thisValue.After(otherValue) {
+			return -1
+		}
+		return 1
+	}
+}
+
+func compareString(thisValue, otherValue string, desc bool) int8 {
+	if desc {
+		if fmt.Sprintf("%v", thisValue) > fmt.Sprintf("%v", otherValue) {
+			return 1
+		}
+		return -1
+	} else {
+		if fmt.Sprintf("%v", thisValue) > fmt.Sprintf("%v", otherValue) {
+			return -1
+		}
+		return 1
+	}
+}
+
+func compareInt64(thisValue, otherValue int64, desc bool) int8 {
+	if desc {
+		if thisValue > otherValue {
+			return 1
+		}
+		return -1
+	} else {
+		if thisValue > otherValue {
+			return -1
+		}
+		return 1
+	}
+}
+
+func compareUint64(thisValue, otherValue uint64, desc bool) int8 {
+	if desc {
+		if thisValue > otherValue {
+			return 1
+		}
+		return -1
+	} else {
+		if thisValue > otherValue {
+			return -1
+		}
+		return 1
+	}
+}
+
+func compareFloat64(thisValue, otherValue float64, desc bool) int8 {
+	if desc {
+		if thisValue > otherValue {
+			return 1
+		}
+		return -1
+	} else {
+		if thisValue > otherValue {
+			return -1
+		}
+		return 1
+	}
 }
