30c4f090381102646c71c755843208aa98f39f53
liaochuntao <liaochuntao@live.com>
2022-07-25 09:37:10 +0800

    feat: support show status (#309)

--- test/integration_test.go
+++ test/integration_test.go
@@ -606,3 +606,36 @@ func (s *IntegrationSuite) TestHints() {
 	}
 
 }
+
+func (s *IntegrationSuite) TestShowStatus() {
+	var (
+		db = s.DB()
+		t  = s.T()
+	)
+
+	type tt struct {
+		sql     string
+		expectF func(t *testing.T, data [][]string) bool
+	}
+
+	for _, it := range []tt{
+		{"SHOW STATUS", func(t *testing.T, data [][]string) bool {
+			t.Logf("%+v", data)
+			return len(data) > 0
+		}},
+		{"SHOW STATUS LIKE 'Key%';", func(t *testing.T, data [][]string) bool {
+			t.Logf("%+v", data)
+			return len(data) >= 5
+		}},
+	} {
+		t.Run(it.sql, func(t *testing.T) {
+			// show table status
+			rows, err := db.Query(it.sql)
+			assert.NoError(t, err, "should query status successfully")
+			defer rows.Close()
+			data, _ := utils.PrintTable(rows)
+			assert.True(t, it.expectF(t, data))
+		})
+	}
+
+}
