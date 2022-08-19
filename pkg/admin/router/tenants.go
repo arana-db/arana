2aa75eb6c986d6db85136cf4fc8dfcb655d7e666
Zhang Aphelios <aphelios.zhang@shopee.com>
2022-08-19 19:53:27 +0800

    add http server api for tenant

--- pkg/admin/router/tenants.go
+++ pkg/admin/router/tenants.go
@@ -18,29 +18,84 @@
 package router
 
 import (
-	"context"
-)
+    "context"
+    "net/http"
 
-import (
-	"github.com/gin-gonic/gin"
-)
+    "github.com/gin-gonic/gin"
 
-import (
-	"github.com/arana-db/arana/pkg/admin"
+    "github.com/arana-db/arana/pkg/admin"
+    "github.com/arana-db/arana/pkg/boot"
 )
 
 func init() {
-	admin.Register(func(router gin.IRoutes) {
-		router.GET("/tenants", ListTenants)
-	})
+    admin.Register(func(router gin.IRoutes) {
+        router.GET("/tenants", ListTenants)
+        router.POST("/tenants", CreateTenant)
+        router.GET("/tenants/:tenant", GetTenant)
+        router.PUT("/tenants/:tenant", UpdateTenant)
+        router.DELETE("/tenants/:tenant", RemoveTenant)
+    })
 }
 
 func ListTenants(c *gin.Context) {
-	service := admin.GetService(c)
-	tenants, err := service.ListTenants(context.Background())
-	if err != nil {
-		_ = c.Error(err)
-		return
-	}
-	c.JSON(200, tenants)
+    service := admin.GetService(c)
+    tenants, err := service.ListTenants(context.Background())
+    if err != nil {
+        _ = c.Error(err)
+        return
+    }
+    c.JSON(http.StatusOK, tenants)
+}
+
+func CreateTenant(c *gin.Context) {
+    service := admin.GetService(c)
+    var tenantBody *boot.TenantBody
+    if err := c.ShouldBindJSON(&tenantBody); err == nil {
+        err := service.UpsertTenant(context.Background(), "", tenantBody)
+        if err != nil {
+            _ = c.Error(err)
+            return
+        }
+        c.JSON(http.StatusCreated, nil)
+    } else {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    }
+}
+
+func GetTenant(c *gin.Context) {
+    service := admin.GetService(c)
+    tenant := c.Param("tenant")
+    data, err := service.GetTenant(context.Background(), tenant)
+    if err != nil {
+        _ = c.Error(err)
+        return
+    }
+    c.JSON(http.StatusOK, data)
+}
+
+func UpdateTenant(c *gin.Context) {
+    service := admin.GetService(c)
+    tenant := c.Param("tenant")
+    var tenantBody *boot.TenantBody
+    if err := c.ShouldBindJSON(&tenantBody); err == nil {
+        err := service.UpsertTenant(context.Background(), tenant, tenantBody)
+        if err != nil {
+            _ = c.Error(err)
+            return
+        }
+        c.JSON(http.StatusOK, nil)
+    } else {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    }
+}
+
+func RemoveTenant(c *gin.Context) {
+    service := admin.GetService(c)
+    tenant := c.Param("tenant")
+    err := service.RemoveTenant(context.Background(), tenant)
+    if err != nil {
+        _ = c.Error(err)
+        return
+    }
+    c.JSON(http.StatusNoContent, nil)
 }
