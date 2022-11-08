12bca9fda607a27bbdfd0f2a9222d50fd6880747
georgehao <haohongfan@gmail.com>
2022-11-08 22:58:44 +0800

    feat: format error string

--- pkg/registry/store/store.go
+++ pkg/registry/store/store.go
@@ -27,21 +27,21 @@ import (
 
 var (
 	// ErrBackendNotSupported is thrown when the backend k/v store is not supported by libkv
-	ErrBackendNotSupported = errors.New("Backend storage not supported yet, please choose one of")
+	ErrBackendNotSupported = errors.New("backend storage not supported yet, please choose one of")
 	// ErrCallNotSupported is thrown when a method is not implemented/supported by the current backend
-	ErrCallNotSupported = errors.New("The current call is not supported with this backend")
+	ErrCallNotSupported = errors.New("the current call is not supported with this backend")
 	// ErrNotReachable is thrown when the API cannot be reached for issuing common store operations
-	ErrNotReachable = errors.New("Api not reachable")
+	ErrNotReachable = errors.New("api not reachable")
 	// ErrCannotLock is thrown when there is an error acquiring a lock on a key
-	ErrCannotLock = errors.New("Error acquiring the lock")
+	ErrCannotLock = errors.New("error acquiring the lock")
 	// ErrKeyModified is thrown during an atomic operation if the index does not match the one in the store
-	ErrKeyModified = errors.New("Unable to complete atomic operation, key modified")
+	ErrKeyModified = errors.New("unable to complete atomic operation, key modified")
 	// ErrKeyNotFound is thrown when the key is not found in the store during a Get operation
-	ErrKeyNotFound = errors.New("Key not found in store")
+	ErrKeyNotFound = errors.New("key not found in store")
 	// ErrPreviousNotSpecified is thrown when the previous value is not specified for an atomic operation
-	ErrPreviousNotSpecified = errors.New("Previous K/V pair should be provided for the Atomic operation")
+	ErrPreviousNotSpecified = errors.New("previous K/V pair should be provided for the Atomic operation")
 	// ErrKeyExists is thrown when the previous value exists in the case of an AtomicPut
-	ErrKeyExists = errors.New("Previous K/V pair exists, cannot complete Atomic operation")
+	ErrKeyExists = errors.New("previous K/V pair exists, cannot complete Atomic operation")
 )
 
 // Options contains the options for a storage client
