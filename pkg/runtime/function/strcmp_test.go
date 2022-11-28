4c92b661be18eda5a43a35ae2978698ac74ab25b
gongna <2036479155@qq.com>
2022-11-28 18:03:52 +0800

    Fix:539

---
+++ pkg/runtime/function/strcmp_test.go
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package function
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/arana-db/arana/pkg/proto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStrcmp(t *testing.T) {
+	fn := proto.MustGetFunc(FuncStrcmp)
+	assert.Equal(t, 2, fn.NumInput())
+	type tt struct {
+		inFirst  proto.Value
+		inSecond proto.Value
+		want     int
+	}
+	for _, v := range []tt{
+		{"text", "text2", -1},
+		{"text2", "text", 1},
+		{"text", "text", 0},
+	} {
+		t.Run(fmt.Sprint(v.inFirst), func(t *testing.T) {
+			out, err := fn.Apply(context.Background(), proto.ToValuer(v.inFirst), proto.ToValuer(v.inSecond))
+			assert.NoError(t, err)
+			assert.Equal(t, v.want, out)
+		})
+	}
+}
