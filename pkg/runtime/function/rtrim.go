cff1aef9e31564d183f1fd144ba4177c2de961cd
gongna <2036479155@qq.com>
2022-12-02 18:41:05 +0800

    [SQL] Support MySQL RTRIM function.

---
+++ pkg/runtime/function/rtrim.go
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package function
+
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+import (
+	"github.com/pkg/errors"
+)
+
+import (
+	"github.com/arana-db/arana/pkg/proto"
+)
+
+// FuncRtrim is  https://dev.mysql.com/doc/refman/5.6/en/string-functions.html#function_rtrim
+const FuncRtrim = "RTRIM"
+
+var _ proto.Func = (*rtrimFunc)(nil)
+
+func init() {
+	proto.RegisterFunc(FuncRtrim, rtrimFunc{})
+}
+
+type rtrimFunc struct{}
+
+func (a rtrimFunc) Apply(ctx context.Context, inputs ...proto.Valuer) (proto.Value, error) {
+	val, err := inputs[0].Value(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	temp := strings.TrimLeft(Reverse(fmt.Sprint(val)), " ")
+	noleftSpaceString := Reverse(temp)
+	return noleftSpaceString, nil
+}
+
+func (a rtrimFunc) NumInput() int {
+	return 1
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
