7876dbe8826cf8b1809765e09f1f21fb65e6caf5
csynineyang <93956978+csynineyang@users.noreply.github.com>
2023-02-09 20:49:30 +0800

    feat: support MySQL CAST_TIME/CAST_DATE/CAST_DATETIME function (#606)
    * add node config support (#464)
    
    * Support MySQL CAST_CHAR function.
    
    * format style
    
    * Support MySQL CAST_TIME function. (#570)
    
    * Support MySQL CAST_DATE function. (#569)
    
    * Support MySQL CAST_DATETIME function. (#568)
    
    * Support MySQL CAST_TIME/CAST_DATE/CAST_DATETIME function
    
    * Resolve Conversation

---
+++ pkg/runtime/function/cast_date.go
@@ -0,0 +1,235 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package function
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+import (
+	"github.com/pkg/errors"
+)
+
+import (
+	"github.com/arana-db/arana/pkg/proto"
+)
+
+// FuncCastDate is  https://dev.mysql.com/doc/refman/5.6/en/cast-functions.html#function_cast
+const FuncCastDate = "CAST_DATE"
+
+var DateSep = `[~!@#$%^&*_+=:;,.|/?\(\)\[\]\{\}\-\\]+`
+var _dateReplace = regexp.MustCompile(DateSep)
+var _dateMatchString = regexp.MustCompile(fmt.Sprintf(`^\d{1,4}%s\d{1,2}%s\d{1,2}$`, DateSep, DateSep))
+var _dateMatchInt = regexp.MustCompile(`^\d{5,8}$`)
+
+var _ proto.Func = (*castDateFunc)(nil)
+
+func init() {
+	proto.RegisterFunc(FuncCastDate, castDateFunc{})
+}
+
+type castDateFunc struct{}
+
+func (a castDateFunc) Apply(ctx context.Context, inputs ...proto.Valuer) (proto.Value, error) {
+	// expr
+	val, err := inputs[0].Value(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	dateArgs := fmt.Sprint(val)
+	if strings.Compare(dateArgs, "") == 0 {
+		return a.DefaultDateValue(), nil
+	}
+
+	// format - YY-MM-DD, YYYY-MM-DD
+	match := _dateMatchString.MatchString(dateArgs)
+	if match {
+		dateArgsReplace := _dateReplace.ReplaceAllStringFunc(dateArgs, func(s string) string { return "-" })
+		dateYear, dateMonth, dateDay := a.splitDateWithSep(dateArgsReplace)
+
+		if a.IsYearValid(dateYear) && a.IsMonthValid(dateMonth) && a.IsDayValid(dateYear, dateMonth, dateDay) {
+			dateStr := a.DateOutput(dateYear, dateMonth, dateDay)
+			return dateStr, nil
+		}
+	}
+	// format - YYYYMMDD, YYMMDD
+	match = _dateMatchInt.MatchString(dateArgs)
+	if match {
+		dateYear, dateMonth, dateDay := a.splitDateWithoutSep(dateArgs)
+
+		if a.IsYearValid(dateYear) && a.IsMonthValid(dateMonth) && a.IsDayValid(dateYear, dateMonth, dateDay) {
+			dateStr := a.DateOutput(dateYear, dateMonth, dateDay)
+			return dateStr, nil
+		}
+	}
+
+	return a.DefaultDateValue(), nil
+}
+
+func (a castDateFunc) NumInput() int {
+	return 1
+}
+
+func (a castDateFunc) DateOutput(year, month, day int) proto.Value {
+	var sb strings.Builder
+
+	yearStr := strconv.FormatInt(int64(year), 10)
+	if year >= 100 && year <= 999 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(yearStr)
+	sb.WriteString("-")
+	monthStr := strconv.FormatInt(int64(month), 10)
+	if month < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(monthStr)
+	sb.WriteString("-")
+	dayStr := strconv.FormatInt(int64(day), 10)
+	if day < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(dayStr)
+
+	return proto.NewValueString(sb.String())
+}
+
+func (a castDateFunc) splitDateWithSep(dateArgs string) (year, month, day int) {
+	dateLen := len(dateArgs)
+	dateDayStr := dateArgs[dateLen-1 : dateLen]
+	dateLeft := dateArgs[0 : dateLen-2]
+	if a.IsDigitalValid(string(dateArgs[dateLen-2])) {
+		dateDayStr = dateArgs[dateLen-2 : dateLen]
+		dateLeft = dateArgs[0 : dateLen-3]
+	}
+	dateArgs = dateLeft
+
+	dateLen = len(dateArgs)
+	dateMonthStr := dateArgs[dateLen-1 : dateLen]
+	dateLeft = dateArgs[0 : dateLen-2]
+	if a.IsDigitalValid(string(dateArgs[dateLen-2])) {
+		dateMonthStr = dateArgs[dateLen-2 : dateLen]
+		dateLeft = dateArgs[0 : dateLen-3]
+	}
+	dateYearStr := dateLeft
+
+	dateYear, _ := strconv.Atoi(dateYearStr)
+	dateYear = a.amend4DigtalYear(dateYear)
+	dateMonth, _ := strconv.Atoi(dateMonthStr)
+	dateDay, _ := strconv.Atoi(dateDayStr)
+
+	return dateYear, dateMonth, dateDay
+}
+
+func (a castDateFunc) splitDateWithoutSep(dateArgs string) (year, month, day int) {
+	dateLen := len(dateArgs)
+	dateDayStr := dateArgs[dateLen-2 : dateLen]
+	dateLeft := dateArgs[0 : dateLen-2]
+	dateArgs = dateLeft
+
+	dateLen = len(dateArgs)
+	dateMonthStr := dateArgs[dateLen-2 : dateLen]
+	dateLeft = dateArgs[0 : dateLen-2]
+	dateYearStr := dateLeft
+
+	dateYear, _ := strconv.Atoi(dateYearStr)
+	dateYear = a.amend4DigtalYear(dateYear)
+	dateMonth, _ := strconv.Atoi(dateMonthStr)
+	dateDay, _ := strconv.Atoi(dateDayStr)
+
+	return dateYear, dateMonth, dateDay
+}
+
+func (a castDateFunc) amend4DigtalYear(year int) int {
+	if year >= 0 && year <= 69 {
+		year += 2000
+	}
+	if year >= 70 && year <= 99 {
+		year += 1900
+	}
+	return year
+}
+
+func (a castDateFunc) IsDigitalValid(data string) bool {
+	if len(data) == 1 && data >= "0" && data <= "9" {
+		return true
+	}
+	return false
+}
+
+func (a castDateFunc) IsYearValid(year int) bool {
+	if year >= 100 && year <= 9999 {
+		return true
+	}
+	return false
+}
+
+func (a castDateFunc) IsMonthValid(month int) bool {
+	if month >= 1 && month <= 12 {
+		return true
+	}
+	return false
+}
+
+func (a castDateFunc) IsDayValid(year, month, day int) bool {
+	if month == 1 || month == 3 || month == 5 || month == 7 ||
+		month == 8 || month == 10 || month == 12 {
+		if day >= 1 && day <= 31 {
+			return true
+		}
+		return false
+	}
+	if month == 4 || month == 6 || month == 9 || month == 11 {
+		if day >= 1 && day <= 30 {
+			return true
+		}
+		return false
+	}
+	if month == 2 {
+		if (year%100 == 0 && year%400 == 0) ||
+			(year%100 > 0 && year%4 == 0) {
+			if day >= 1 && day <= 29 {
+				return true
+			}
+			return false
+		} else {
+			if day >= 1 && day <= 28 {
+				return true
+			}
+			return false
+		}
+	}
+	return false
+}
+
+func (a castDateFunc) MaxDateValue() proto.Value {
+	return proto.NewValueString("9999-12-31")
+}
+
+func (a castDateFunc) MinDateValue() proto.Value {
+	return proto.NewValueString("0000-01-01")
+}
+
+func (a castDateFunc) DefaultDateValue() proto.Value {
+	return proto.NewValueString("0000-00-00")
+}
---
+++ pkg/runtime/function/cast_time.go
@@ -0,0 +1,258 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package function
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+import (
+	"github.com/pkg/errors"
+)
+
+import (
+	"github.com/arana-db/arana/pkg/proto"
+)
+
+// FuncCastTime is  https://dev.mysql.com/doc/refman/5.6/en/cast-functions.html#function_cast
+const FuncCastTime = "CAST_TIME"
+
+var _timeMatchDay = regexp.MustCompile(`^\d{1,2} \d{1,3}(:\d{1,2}){0,2}$`)
+var _timeMatchString = regexp.MustCompile(`^\d{1,3}(:\d{1,2}){1,2}$`)
+var _timeMatchInt = regexp.MustCompile(`^\d{1,7}$`)
+
+var _ proto.Func = (*castTimeFunc)(nil)
+
+func init() {
+	proto.RegisterFunc(FuncCastTime, castTimeFunc{})
+}
+
+type castTimeFunc struct{}
+
+func (a castTimeFunc) Apply(ctx context.Context, inputs ...proto.Valuer) (proto.Value, error) {
+	// expr
+	val, err := inputs[0].Value(ctx)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	timeArgs := fmt.Sprint(val)
+	if strings.Compare(timeArgs, "") == 0 {
+		return a.DefaultTimeValue(), nil
+	}
+
+	// negative flag
+	nega := false
+	if strings.Compare(string(timeArgs[0]), "-") == 0 {
+		nega = true
+		timeArgs = timeArgs[1:]
+	}
+	if strings.Compare(string(timeArgs[0]), "+") == 0 {
+		timeArgs = timeArgs[1:]
+	}
+
+	// fractional seconds
+	frac := false
+	if strings.Contains(timeArgs, ".") {
+		timeArr := strings.Split(timeArgs, ".")
+		if len(timeArr) >= 2 && len(timeArr[1]) >= 1 {
+			timeFrac, _ := strconv.Atoi(string(timeArr[1][0]))
+			if timeFrac >= 5 {
+				frac = true
+			}
+		}
+		timeArgs = timeArr[0]
+	}
+
+	if strings.Contains(timeArgs, " ") {
+		// format - D hhh:mm:ss.ms，D hhh:mm.ms，D hhh.ms
+		match := _timeMatchDay.MatchString(timeArgs)
+		if !match {
+			return a.DefaultTimeValue(), nil
+		}
+
+		timeArrLeft := strings.Split(timeArgs, " ")
+		timeDay, _ := strconv.Atoi(timeArrLeft[0])
+		timeHour, timeMinutes, timeSecond := a.splitTimeWithSep(timeArrLeft[1])
+		timeHour += timeDay * 24
+
+		if !a.IsHourValid(timeHour) {
+			return a.MaxTimeValue(), nil
+		}
+		if a.IsMinutesValid(timeMinutes) && a.IsSecondValid(timeSecond) {
+			timeStr := a.TimeOutput(timeHour, timeMinutes, timeSecond, nega, frac)
+			return timeStr, nil
+		}
+	} else if strings.Contains(timeArgs, ":") {
+		// format - hhh:mm:ss.ms，hhh:mm.ms
+		match := _timeMatchString.MatchString(timeArgs)
+		if !match {
+			return a.DefaultTimeValue(), nil
+		}
+
+		timeHour, timeMinutes, timeSecond := a.splitTimeWithSep(timeArgs)
+
+		if !a.IsHourValid(timeHour) {
+			return a.MaxTimeValue(), nil
+		}
+		if a.IsMinutesValid(timeMinutes) && a.IsSecondValid(timeSecond) {
+			timeStr := a.TimeOutput(timeHour, timeMinutes, timeSecond, nega, frac)
+			return timeStr, nil
+		}
+	} else {
+		// format - hhhmmss.ms，mmss.ms，ss.ms
+		match := _timeMatchInt.MatchString(timeArgs)
+		if !match {
+			return a.DefaultTimeValue(), nil
+		}
+
+		timeHour, timeMinutes, timeSecond := a.splitTimeWithoutSep(timeArgs)
+
+		if !a.IsHourValid(timeHour) {
+			return a.MaxTimeValue(), nil
+		}
+		if a.IsMinutesValid(timeMinutes) && a.IsSecondValid(timeSecond) {
+			timeStr := a.TimeOutput(timeHour, timeMinutes, timeSecond, nega, frac)
+			return timeStr, nil
+		}
+	}
+
+	return a.DefaultTimeValue(), nil
+}
+
+func (a castTimeFunc) NumInput() int {
+	return 1
+}
+
+func (a castTimeFunc) TimeOutput(hour, minutes, second int, nega, frac bool) proto.Value {
+	if hour == 838 && minutes == 59 && second == 59 {
+		timeStr := a.MaxTimeValue()
+		if nega {
+			timeStr = a.MinTimeValue()
+		}
+		return timeStr
+	}
+
+	if frac {
+		second += 1
+		if second >= 60 {
+			minutes += 1
+			second = 0
+		}
+		if minutes >= 60 {
+			hour += 1
+			minutes = 0
+		}
+	}
+
+	var sb strings.Builder
+	if nega {
+		sb.WriteString("-")
+	}
+	hourStr := strconv.FormatInt(int64(hour), 10)
+	if hour < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(hourStr)
+	sb.WriteString(":")
+	minutesStr := strconv.FormatInt(int64(minutes), 10)
+	if minutes < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(minutesStr)
+	sb.WriteString(":")
+	secondStr := strconv.FormatInt(int64(second), 10)
+	if second < 10 {
+		sb.WriteString("0")
+	}
+	sb.WriteString(secondStr)
+
+	return proto.NewValueString(sb.String())
+}
+
+func (a castTimeFunc) splitTimeWithSep(timeArgs string) (hour, minutes, second int) {
+	timeArr := strings.Split(timeArgs, ":")
+	timeHour := 0
+	if len(timeArr) >= 1 {
+		timeHour, _ = strconv.Atoi(timeArr[0])
+	}
+	timeMinutes := 0
+	if len(timeArr) >= 2 {
+		timeMinutes, _ = strconv.Atoi(timeArr[1])
+	}
+	timeSecond := 0
+	if len(timeArr) >= 3 {
+		timeSecond, _ = strconv.Atoi(timeArr[2])
+	}
+
+	return timeHour, timeMinutes, timeSecond
+}
+
+func (a castTimeFunc) splitTimeWithoutSep(timeArgs string) (hour, minutes, second int) {
+	timeInt, _ := strconv.Atoi(timeArgs)
+	timeSecond := timeInt % 100
+	timeLeft := timeInt / 100
+	timeMinutes := timeLeft % 100
+	timeHour := timeLeft / 100
+
+	return timeHour, timeMinutes, timeSecond
+}
+
+func (a castTimeFunc) IsDayValid(day int) bool {
+	if day >= 0 && day <= 34 {
+		return true
+	}
+	return false
+}
+
+func (a castTimeFunc) IsHourValid(hour int) bool {
+	if hour >= 0 && hour <= 838 {
+		return true
+	}
+	return false
+}
+
+func (a castTimeFunc) IsMinutesValid(minutes int) bool {
+	if minutes >= 0 && minutes <= 59 {
+		return true
+	}
+	return false
+}
+
+func (a castTimeFunc) IsSecondValid(second int) bool {
+	if second >= 0 && second <= 59 {
+		return true
+	}
+	return false
+}
+
+func (a castTimeFunc) MaxTimeValue() proto.Value {
+	return proto.NewValueString("838:59:59")
+}
+
+func (a castTimeFunc) MinTimeValue() proto.Value {
+	return proto.NewValueString("-838:59:59")
+}
+
+func (a castTimeFunc) DefaultTimeValue() proto.Value {
+	return proto.NewValueString("00:00:00")
+}
