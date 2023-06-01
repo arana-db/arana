e0b88ddf26f784004debe46547763297b4181126
wang1309 <1028960576@qq.com>
2023-06-01 08:29:01 +0800

    temp commit

--- pkg/dataset/sort_merge_join.go
+++ pkg/dataset/sort_merge_join.go
@@ -18,28 +18,176 @@
 package dataset
 
 import (
+	"go.uber.org/atomic"
 	"io"
 	"strings"
+	"sync"
 )
 
 import (
 	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 )
 
 import (
 	"github.com/arana-db/arana/pkg/mysql/rows"
 	"github.com/arana-db/arana/pkg/proto"
 	"github.com/arana-db/arana/pkg/runtime/ast"
+	"github.com/arana-db/arana/pkg/util/log"
 )
 
 var _ proto.Dataset = (*SortMergeJoin)(nil)
 
+const (
+	IsDescartes  = true
+	NotDescartes = false
+)
+
+// SortMergeJoin assume all outer data and inner data are sorted by join column
 type SortMergeJoin struct {
+	// fields is the union of outer fields and inner fields
 	fields     []proto.Field
 	joinColumn *JoinColumn
-	joinType   ast.JoinType
-	outer      proto.Dataset
-	inner      proto.Dataset
+	// joinType inner join, left join, right join
+	joinType  ast.JoinType
+	outer     proto.Dataset
+	inner     proto.Dataset
+	beforeRow proto.Row
+	// equalValue when outer value equal inner value, set this value use to generate descartes product
+	equalValue map[string][]proto.Row
+	// equalIndex record the index of equalValue visited position
+	equalIndex map[string]int
+	// descartesFlag
+	descartesFlag atomic.Bool
+	rwLock        sync.RWMutex
+}
+
+func NewSortMergeJoin(joinType ast.JoinType, joinColumn *JoinColumn, outer proto.Dataset, inner proto.Dataset) (*SortMergeJoin, error) {
+	outerFields, err := outer.Fields()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	innerFields, err := inner.Fields()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	fields := make([]proto.Field, 0, len(outerFields)+len(innerFields))
+	fields = append(fields, outerFields...)
+	fields = append(fields, innerFields...)
+
+	return &SortMergeJoin{
+		fields:     fields,
+		joinColumn: joinColumn,
+		joinType:   joinType,
+		outer:      outer,
+		inner:      inner,
+		equalValue: make(map[string][]proto.Row),
+		equalIndex: make(map[string]int),
+	}, nil
+}
+
+func (s *SortMergeJoin) SetBeforeRow(v proto.Row) {
+	if s != nil {
+		s.beforeRow = v
+	}
+}
+
+func (s *SortMergeJoin) BeforeRow() proto.Row {
+	if s != nil {
+		return s.beforeRow
+	}
+
+	return nil
+}
+
+func (s *SortMergeJoin) CleanBeforeRow() {
+	if s != nil {
+		s.beforeRow = nil
+	}
+}
+
+func (s *SortMergeJoin) SetEqualValue(key string, value proto.Row) {
+	if s != nil {
+		s.rwLock.Lock()
+		defer s.rwLock.Unlock()
+		s.equalValue[key] = append(s.equalValue[key], value)
+		if _, ok := s.equalIndex[key]; !ok {
+			s.equalIndex[key] = 0
+		}
+	}
+}
+
+func (s *SortMergeJoin) EqualValue(key string) proto.Row {
+	if s != nil {
+		s.rwLock.RLock()
+		defer s.rwLock.RUnlock()
+
+		if v, ok := s.equalValue[key]; ok {
+			res := v[s.equalIndex[key]]
+			index := s.equalIndex[key] + 1
+			if index < len(v) {
+				s.equalIndex[key] = index
+			} else {
+				s.equalIndex[key] = 0
+			}
+			return res
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func (s *SortMergeJoin) EqualValueLen(key string) int {
+	if s != nil {
+		s.rwLock.RLock()
+		defer s.rwLock.RUnlock()
+
+		if v, ok := s.equalValue[key]; ok {
+			return len(v)
+		}
+	}
+
+	return 0
+}
+
+func (s *SortMergeJoin) EqualIndex(key string) int {
+	if s != nil {
+		s.rwLock.RLock()
+		defer s.rwLock.RUnlock()
+
+		if v, ok := s.equalIndex[key]; ok {
+			return v
+		}
+	}
+
+	return 0
+}
+
+func (s *SortMergeJoin) isDescartes(key string) bool {
+	s.rwLock.RLock()
+	defer s.rwLock.RUnlock()
+	if _, ok := s.equalValue[key]; ok {
+		return true
+	}
+
+	return false
+}
+
+func (s *SortMergeJoin) setDescartesFlag(flag bool) {
+	if s != nil {
+		s.descartesFlag.Store(flag)
+	}
+}
+
+func (s *SortMergeJoin) DescartesFlag() bool {
+	if s != nil {
+		return s.descartesFlag.Load()
+	}
+
+	return false
 }
 
 type JoinColumn struct {
@@ -63,14 +211,404 @@ func (s *SortMergeJoin) Fields() ([]proto.Field, error) {
 }
 
 func (s *SortMergeJoin) Next() (proto.Row, error) {
-	// all data is order
+	var (
+		err                error
+		outerRow, innerRow proto.Row
+	)
 
-	outerRow, err := s.getOuterRow()
+	if s.BeforeRow() != nil {
+		outerRow = s.BeforeRow()
+	} else {
+		outerRow, err = s.getOuterRow()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	innerRow, err = s.getInnerRow(outerRow)
 	if err != nil {
 		return nil, err
 	}
 
-	innerRow, err := s.getInnerRow()
+	switch s.joinType {
+	case ast.InnerJoin:
+		return s.innerJoin(outerRow, innerRow)
+	case ast.LeftJoin:
+		return s.leftJoin(outerRow, innerRow)
+	case ast.RightJoin:
+		return s.rightJoin()
+	default:
+		return nil, errors.New("not support join type")
+	}
+}
+
+// innerJoin
+func (s *SortMergeJoin) innerJoin(outerRow proto.Row, innerRow proto.Row) (proto.Row, error) {
+	var (
+		err                    error
+		outerValue, innerValue proto.Value
+	)
+
+	for {
+		if outerRow == nil || innerRow == nil {
+			return nil, io.EOF
+		}
+
+		outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		innerValue, err = innerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		if proto.CompareValue(outerValue, innerValue) == 0 {
+			// restore last value
+			// example : 1,2,2 => 2,2,3
+			// first left 2 match right first 2
+			// next still need left 2  match next second 2
+			s.SetBeforeRow(outerRow)
+			if s.DescartesFlag() {
+				index := s.EqualIndex(outerValue.String())
+				if index == 0 {
+					nextOuterRow, err := s.getOuterRow()
+					if err != nil {
+						return nil, err
+					}
+					s.SetBeforeRow(nextOuterRow)
+				}
+			}
+
+			if !s.DescartesFlag() {
+				s.SetEqualValue(cast.ToString(outerValue), innerRow)
+			}
+
+			return s.resGenerate(outerRow, innerRow), nil
+		}
+
+		if proto.CompareValue(outerValue, innerValue) < 0 {
+			s.CleanBeforeRow()
+			outerRow, err = s.getOuterRow()
+			if err != nil {
+				return nil, err
+			}
+
+			if outerRow == nil {
+				return nil, io.EOF
+			}
+
+			// if outer row equal last row, do descartes match
+			outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+			if err != nil {
+				return nil, err
+			}
+
+			s.setDescartesFlag(NotDescartes)
+			if s.isDescartes(outerValue.String()) {
+				s.setDescartesFlag(IsDescartes)
+				innerRow = s.EqualValue(outerValue.String())
+			}
+		}
+
+		if proto.CompareValue(outerValue, innerValue) > 0 {
+			s.CleanBeforeRow()
+			// if outer row equal last row, do descartes match
+			outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+			if err != nil {
+				return nil, err
+			}
+
+			if outerRow == nil {
+				return nil, io.EOF
+			}
+
+			if s.isDescartes(outerValue.String()) {
+				s.setDescartesFlag(IsDescartes)
+				innerRow = s.EqualValue(outerValue.String())
+			} else {
+				s.setDescartesFlag(NotDescartes)
+				innerRow, err = s.getInnerRow(outerRow)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+}
+
+/*func (s *SortMergeJoin) innerJoin() (proto.Row, error) {
+	var (
+		err                error
+		outerRow, innerRow proto.Row
+	)
+
+	if s.BeforeRow() != nil {
+		outerRow = s.BeforeRow()
+	} else {
+		outerRow, err = s.getOuterRow()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	innerRow, err = s.getInnerRow(outerRow)
+	if err != nil {
+		return nil, err
+	}
+
+	var outerValue, innerValue proto.Value
+
+	for {
+		if outerRow == nil || innerRow == nil {
+			return nil, io.EOF
+		}
+
+		outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		innerValue, err = innerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		if proto.CompareValue(outerValue, innerValue) == 0 {
+			// restore last value
+			// example : 1,2,2 => 2,2,3
+			// first left 2 match right first 2
+			// next still need left 2  match next second 2
+			s.SetBeforeRow(outerRow)
+			if s.DescartesFlag() {
+				index := s.EqualIndex(outerValue.String())
+				if index == 0 {
+					nextOuterRow, err := s.getOuterRow()
+					if err != nil {
+						return nil, err
+					}
+					s.SetBeforeRow(nextOuterRow)
+				}
+			}
+
+			if !s.DescartesFlag() {
+				s.SetEqualValue(cast.ToString(outerValue), innerRow)
+			}
+
+			return s.resGenerate(outerRow, innerRow), nil
+		}
+
+		if proto.CompareValue(outerValue, innerValue) < 0 {
+			s.CleanBeforeRow()
+			outerRow, err = s.getOuterRow()
+			if err != nil {
+				return nil, err
+			}
+
+			if outerRow == nil {
+				return nil, io.EOF
+			}
+
+			// if outer row equal last row, do descartes match
+			outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+			if err != nil {
+				return nil, err
+			}
+
+			s.setDescartesFlag(NotDescartes)
+			if s.isDescartes(cast.ToString(outerValue)) {
+				s.setDescartesFlag(IsDescartes)
+				innerRow = s.EqualValue(cast.ToString(outerValue))
+			}
+		}
+
+		if proto.CompareValue(outerValue, innerValue) > 0 {
+			s.CleanBeforeRow()
+			// if outer row equal last row, do descartes match
+			outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+			if err != nil {
+				return nil, err
+			}
+
+			if outerRow == nil {
+				return nil, io.EOF
+			}
+
+			if s.isDescartes(cast.ToString(outerValue)) {
+				s.setDescartesFlag(IsDescartes)
+				innerRow = s.EqualValue(cast.ToString(outerValue))
+			} else {
+				s.setDescartesFlag(NotDescartes)
+				innerRow, err = s.getInnerRow(outerRow)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+	}
+}*/
+
+func (s *SortMergeJoin) leftJoin(outerRow proto.Row, innerRow proto.Row) (proto.Row, error) {
+	var (
+		err                    error
+		outerValue, innerValue proto.Value
+	)
+
+	for {
+		if outerRow == nil || innerRow == nil {
+			return nil, io.EOF
+		}
+
+		outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		innerValue, err = innerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		if proto.CompareValue(outerValue, innerValue) == 0 {
+			// restore last value
+			// example : 1,2,2 => 2,2,3
+			// first left 2 match right first 2
+			// next still need left 2  match next second 2
+			s.SetBeforeRow(outerRow)
+			if s.DescartesFlag() {
+				index := s.EqualIndex(outerValue.String())
+				if index == 0 {
+					nextOuterRow, err := s.getOuterRow()
+					if err != nil {
+						return nil, err
+					}
+					s.SetBeforeRow(nextOuterRow)
+				}
+			}
+
+			if !s.DescartesFlag() {
+				s.SetEqualValue(cast.ToString(outerValue), innerRow)
+			}
+
+			return s.resGenerate(outerRow, innerRow), nil
+		}
+
+		if proto.CompareValue(outerValue, innerValue) < 0 {
+			s.CleanBeforeRow()
+			outerRow, err = s.getOuterRow()
+			if err != nil {
+				return nil, err
+			}
+
+			if outerRow == nil {
+				return nil, io.EOF
+			}
+
+			// if outer row equal last row, do descartes match
+			outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+			if err != nil {
+				return nil, err
+			}
+
+			s.setDescartesFlag(NotDescartes)
+			if s.isDescartes(outerValue.String()) {
+				s.setDescartesFlag(IsDescartes)
+				innerRow = s.EqualValue(outerValue.String())
+			} else {
+				return s.resGenerate(outerRow, nil), nil
+			}
+		}
+
+		if proto.CompareValue(outerValue, innerValue) > 0 {
+			s.CleanBeforeRow()
+			// if outer row equal last row, do descartes match
+			outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+			if err != nil {
+				return nil, err
+			}
+
+			if outerRow == nil {
+				return nil, io.EOF
+			}
+
+			if s.isDescartes(outerValue.String()) {
+				s.setDescartesFlag(IsDescartes)
+				innerRow = s.EqualValue(outerValue.String())
+			} else {
+				s.setDescartesFlag(NotDescartes)
+				return s.resGenerate(outerRow, nil), nil
+				/*innerRow, err = s.getInnerRow(outerRow)
+				if err != nil {
+					return nil, err
+				}*/
+			}
+		}
+	}
+}
+
+// leftJoin
+/*func (s *SortMergeJoin) leftJoin(outerRow proto.Row, innerRow proto.Row) (proto.Row, error) {
+	var (
+		err                    error
+		outerValue, innerValue proto.Value
+	)
+
+	for {
+		if outerRow == nil && innerRow == nil {
+			return nil, nil
+		}
+
+		outerValue, err = outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		innerValue, err = innerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+		if err != nil {
+			return nil, err
+		}
+
+		if strings.Compare(outerValue.String(), innerValue.String()) == 0 {
+			// restore last value
+			// example : 1,2,2 => 2,2,3
+			// first left 2 match right first 2
+			// next still need left 2  match next second 2
+			s.SetBeforeRow(outerRow)
+			return s.resGenerate(outerRow, innerRow), nil
+		}
+
+		if strings.Compare(outerValue.String(), innerValue.String()) < 0 {
+			s.SetBeforeRow(innerRow)
+			// return left row + null
+			return s.resGenerate(outerRow, nil), nil
+		}
+
+		if strings.Compare(outerValue.String(), innerValue.String()) > 0 {
+			s.SetBeforeRow(outerRow)
+			// return right row + null
+			return s.resGenerate(outerRow, nil), nil
+		}
+	}
+}*/
+
+func (s *SortMergeJoin) rightJoin() (proto.Row, error) {
+	var (
+		err                error
+		outerRow, innerRow proto.Row
+	)
+
+	// all data is order
+	if s.BeforeRow() != nil {
+		outerRow = s.BeforeRow()
+	} else {
+		outerRow, err = s.getOuterRow()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	innerRow, err = s.getInnerRow(outerRow)
 	if err != nil {
 		return nil, err
 	}
@@ -93,18 +631,24 @@ func (s *SortMergeJoin) Next() (proto.Row, error) {
 		}
 
 		if strings.Compare(outerValue.String(), innerValue.String()) == 0 {
+			// restore last value
+			// example : 1,2,2 => 2,2,3
+			// first left 2 match right first 2
+			// next still need left 2  match next second 2
+			s.SetBeforeRow(outerRow)
 			return s.resGenerate(outerRow, innerRow), nil
 		}
 
 		if strings.Compare(outerValue.String(), innerValue.String()) < 0 {
+			s.SetBeforeRow(innerRow)
+			// return left row + null
 			return s.resGenerate(outerRow, nil), nil
 		}
 
 		if strings.Compare(outerValue.String(), innerValue.String()) > 0 {
-			innerRow, err = s.getInnerRow()
-			if err != nil {
-				return nil, err
-			}
+			s.SetBeforeRow(outerRow)
+			// return right row + null
+			return s.resGenerate(innerRow, nil), nil
 		}
 	}
 }
@@ -121,7 +665,16 @@ func (s *SortMergeJoin) getOuterRow() (proto.Row, error) {
 	return leftRow, nil
 }
 
-func (s *SortMergeJoin) getInnerRow() (proto.Row, error) {
+func (s *SortMergeJoin) getInnerRow(outerRow proto.Row) (proto.Row, error) {
+	outerValue, err := outerRow.(proto.KeyedRow).Get(s.joinColumn.Column())
+	if err != nil {
+		return nil, err
+	}
+
+	if s.DescartesFlag() {
+		return s.EqualValue(outerValue.String()), nil
+	}
+
 	rightRow, err := s.inner.Next()
 	if err != nil && errors.Is(err, io.EOF) {
 		return nil, nil
@@ -134,15 +687,55 @@ func (s *SortMergeJoin) getInnerRow() (proto.Row, error) {
 }
 
 func (s *SortMergeJoin) resGenerate(leftRow proto.Row, rightRow proto.Row) proto.Row {
-	res := make([]proto.Value, leftRow.Length()+rightRow.Length())
-	leftValue := make([]proto.Value, leftRow.Length())
-	rightValue := make([]proto.Value, rightRow.Length())
+	var (
+		leftValue  []proto.Value
+		rightValue []proto.Value
+		res        []proto.Value
+	)
 
-	_ = leftRow.Scan(leftValue)
-	_ = rightRow.Scan(rightValue)
+	if leftRow == nil && rightRow == nil {
+		return nil
+	}
+
+	leftFields, _ := s.outer.Fields()
+	rightFields, _ := s.inner.Fields()
+
+	leftValue = make([]proto.Value, len(leftFields))
+	rightValue = make([]proto.Value, len(rightFields))
+
+	if leftRow == nil {
+		if err := rightRow.(proto.KeyedRow).Scan(rightValue); err != nil {
+			log.Infof("left row scan error, err: %+v", err)
+			return nil
+		}
+	}
+
+	if rightRow == nil {
+		if err := leftRow.(proto.KeyedRow).Scan(leftValue); err != nil {
+			log.Infof("left row scan error, err: %+v", err)
+			return nil
+		}
+	}
+
+	if leftRow != nil && rightRow != nil {
+		if err := leftRow.(proto.KeyedRow).Scan(leftValue); err != nil {
+			log.Infof("left row scan error, err: %+v", err)
+			return nil
+		}
+		if err := rightRow.(proto.KeyedRow).Scan(rightValue); err != nil {
+			log.Infof("left row scan error, err: %+v", err)
+			return nil
+		}
+	}
 
 	res = append(res, leftValue...)
 	res = append(res, rightValue...)
 
-	return rows.NewBinaryVirtualRow(s.fields, res)
+	fields, _ := s.Fields()
+
+	return rows.NewBinaryVirtualRow(fields, res)
 }
+
+/*func (s *SortMergeJoin) getDescartesRow() proto.Row {
+
+}*/
--- pkg/dataset/sort_merge_join_test.go
+++ pkg/dataset/sort_merge_join_test.go
@@ -1,7 +1,3 @@
-package dataset
-
-import "testing"
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -19,6 +15,395 @@ import "testing"
  * limitations under the License.
  */
 
-func TestSortMergeJoin_Next(t *testing.T) {
+package dataset
 
+import (
+	"fmt"
+	"io"
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+import (
+	constants "github.com/arana-db/arana/pkg/constants/mysql"
+	"github.com/arana-db/arana/pkg/mysql"
+	"github.com/arana-db/arana/pkg/mysql/rows"
+	"github.com/arana-db/arana/pkg/proto"
+	"github.com/arana-db/arana/pkg/runtime/ast"
+)
+
+func TestInnerSortMergeJoin_Next(t *testing.T) {
+	fieldsAll := []proto.Field{
+		mysql.NewField("id", constants.FieldTypeLong),
+		mysql.NewField("name", constants.FieldTypeVarChar),
+		mysql.NewField("gender", constants.FieldTypeLong),
+		mysql.NewField("id", constants.FieldTypeLong),
+		mysql.NewField("name", constants.FieldTypeVarChar),
+		mysql.NewField("gender", constants.FieldTypeLong),
+	}
+
+	field := []proto.Field{
+		mysql.NewField("id", constants.FieldTypeLong),
+		mysql.NewField("name", constants.FieldTypeVarChar),
+		mysql.NewField("gender", constants.FieldTypeLong),
+	}
+
+	// test case 1: outer value [0,1,2,3,4,5,6,7,8,9], inner value [5,6,7,8,9,10,11,12,13,14]
+	outer1 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(0); i < 10; i++ {
+		outer1.Rows = append(outer1.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	inner1 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(5); i < 15; i++ {
+		inner1.Rows = append(inner1.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	// test case 2: outer value [2,2,3], inner value [2,2,4]
+	outer2 := &VirtualDataset{
+		Columns: field,
+	}
+
+	outer2.Rows = append(outer2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%d", 2)),
+		proto.NewValueInt64(0), // 0=female,1=male
+	}))
+
+	outer2.Rows = append(outer2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 2.1)),
+		proto.NewValueInt64(1), // 0=female,1=male
+	}))
+
+	outer2.Rows = append(outer2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(3),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 2.2)),
+		proto.NewValueInt64(1), // 0=female,1=male
+	}))
+
+	inner2 := &VirtualDataset{
+		Columns: field,
+	}
+
+	inner2.Rows = append(inner2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%d", 3)),
+		proto.NewValueInt64(0), // 0=female,1=male
+	}))
+
+	inner2.Rows = append(inner2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 3.1)),
+		proto.NewValueInt64(1), // 0=female,1=male
+	}))
+
+	inner2.Rows = append(inner2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(4),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 3.2)),
+		proto.NewValueInt64(0), // 0=female,1=male
+	}))
+
+	// test case 3: outer value [1,2,3], inner value [4,5,6]
+	outer3 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(1); i < 4; i++ {
+		outer3.Rows = append(outer3.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	inner3 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(4); i < 7; i++ {
+		inner3.Rows = append(inner3.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	type fields struct {
+		fields     []proto.Field
+		joinColumn *JoinColumn
+		joinType   ast.JoinType
+		outer      proto.Dataset
+		inner      proto.Dataset
+		beforeRow  proto.Row
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    proto.Row
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "test case 1",
+			fields: fields{
+				fields: fieldsAll,
+				joinColumn: &JoinColumn{
+					column: "id",
+				},
+				joinType: ast.InnerJoin,
+				outer:    outer1,
+				inner:    inner1,
+			},
+		},
+		{
+			name: "test case 2",
+			fields: fields{
+				fields: fieldsAll,
+				joinColumn: &JoinColumn{
+					column: "id",
+				},
+				joinType: ast.InnerJoin,
+				outer:    outer2,
+				inner:    inner2,
+			},
+		},
+		{
+			name: "test case 3",
+			fields: fields{
+				fields: fieldsAll,
+				joinColumn: &JoinColumn{
+					column: "id",
+				},
+				joinType: ast.InnerJoin,
+				outer:    outer3,
+				inner:    inner3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := NewSortMergeJoin(tt.fields.joinType, tt.fields.joinColumn, tt.fields.outer, tt.fields.inner)
+			/*s := &SortMergeJoin{
+				fields:     tt.fields.fields,
+				joinColumn: tt.fields.joinColumn,
+				joinType:   tt.fields.joinType,
+				outer:      tt.fields.outer,
+				inner:      tt.fields.inner,
+				beforeRow:  tt.fields.beforeRow,
+			}*/
+
+			for {
+				row, err := s.Next()
+				if err == io.EOF {
+					return
+				}
+
+				if row != nil {
+					dest := make([]proto.Value, len(fieldsAll))
+					err = row.Scan(dest)
+					assert.NoError(t, err)
+					t.Logf("id=%v, name=%v, gender=%v, id1=%v, name1=%v, gender1=%v\n", dest[0], dest[1], dest[2], dest[3], dest[4], dest[5])
+				}
+			}
+		})
+	}
+}
+
+func TestLeftSortMergeJoin_Next(t *testing.T) {
+	fieldsAll := []proto.Field{
+		mysql.NewField("id", constants.FieldTypeLong),
+		mysql.NewField("name", constants.FieldTypeVarChar),
+		mysql.NewField("gender", constants.FieldTypeLong),
+		mysql.NewField("id", constants.FieldTypeLong),
+		mysql.NewField("name", constants.FieldTypeVarChar),
+		mysql.NewField("gender", constants.FieldTypeLong),
+	}
+
+	field := []proto.Field{
+		mysql.NewField("id", constants.FieldTypeLong),
+		mysql.NewField("name", constants.FieldTypeVarChar),
+		mysql.NewField("gender", constants.FieldTypeLong),
+	}
+
+	// test case 1: outer value [0,1,2,3,4,5,6,7,8,9], inner value [5,6,7,8,9,10,11,12,13,14]
+	outer1 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(0); i < 10; i++ {
+		outer1.Rows = append(outer1.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	inner1 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(5); i < 15; i++ {
+		inner1.Rows = append(inner1.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	// test case 2: outer value [2,2,3], inner value [2,2,4]
+	outer2 := &VirtualDataset{
+		Columns: field,
+	}
+
+	outer2.Rows = append(outer2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%d", 2)),
+		proto.NewValueInt64(0), // 0=female,1=male
+	}))
+
+	outer2.Rows = append(outer2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 2.1)),
+		proto.NewValueInt64(1), // 0=female,1=male
+	}))
+
+	outer2.Rows = append(outer2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(3),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 2.2)),
+		proto.NewValueInt64(1), // 0=female,1=male
+	}))
+
+	inner2 := &VirtualDataset{
+		Columns: field,
+	}
+
+	inner2.Rows = append(inner2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%d", 3)),
+		proto.NewValueInt64(0), // 0=female,1=male
+	}))
+
+	inner2.Rows = append(inner2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(2),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 3.1)),
+		proto.NewValueInt64(1), // 0=female,1=male
+	}))
+
+	inner2.Rows = append(inner2.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+		proto.NewValueInt64(4),
+		proto.NewValueString(fmt.Sprintf("fake-name-%f", 3.2)),
+		proto.NewValueInt64(0), // 0=female,1=male
+	}))
+
+	// test case 3: outer value [1,2,3], inner value [4,5,6]
+	outer3 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(1); i < 4; i++ {
+		outer3.Rows = append(outer3.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	inner3 := &VirtualDataset{
+		Columns: field,
+	}
+
+	for i := int64(4); i < 7; i++ {
+		inner3.Rows = append(inner3.Rows, rows.NewTextVirtualRow(field, []proto.Value{
+			proto.NewValueInt64(i),
+			proto.NewValueString(fmt.Sprintf("fake-name-%d", i)),
+			proto.NewValueInt64(i & 1), // 0=female,1=male
+		}))
+	}
+
+	type fields struct {
+		fields     []proto.Field
+		joinColumn *JoinColumn
+		joinType   ast.JoinType
+		outer      proto.Dataset
+		inner      proto.Dataset
+		beforeRow  proto.Row
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    proto.Row
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "test case 4",
+			fields: fields{
+				fields: fieldsAll,
+				joinColumn: &JoinColumn{
+					column: "id",
+				},
+				joinType: ast.LeftJoin,
+				outer:    outer1,
+				inner:    inner1,
+			},
+		},
+		{
+			name: "test case 5",
+			fields: fields{
+				fields: fieldsAll,
+				joinColumn: &JoinColumn{
+					column: "id",
+				},
+				joinType: ast.LeftJoin,
+				outer:    outer2,
+				inner:    inner2,
+			},
+		},
+		{
+			name: "test case 6",
+			fields: fields{
+				fields: fieldsAll,
+				joinColumn: &JoinColumn{
+					column: "id",
+				},
+				joinType: ast.LeftJoin,
+				outer:    outer3,
+				inner:    inner3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := NewSortMergeJoin(tt.fields.joinType, tt.fields.joinColumn, tt.fields.outer, tt.fields.inner)
+			for {
+				row, err := s.Next()
+				if err == io.EOF {
+					return
+				}
+
+				if row != nil {
+					dest := make([]proto.Value, len(fieldsAll))
+					err = row.Scan(dest)
+					assert.NoError(t, err)
+					t.Logf("id=%v, name=%v, gender=%v, id1=%v, name1=%v, gender1=%v\n", dest[0], dest[1], dest[2], dest[3], dest[4], dest[5])
+				}
+			}
+		})
+	}
 }
